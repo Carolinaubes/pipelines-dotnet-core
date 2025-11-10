09148904d2b4252522f189a059b81b9e34ff7a29
Carolina Uribe Builes <91623368+Carolinaubes@users.noreply.github.com>
2025-11-10 16:36:14 -0500

    Add basic unit test for addition

---
+++ Tests/pipelines-tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace pipelines_tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void SumaBasica()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
