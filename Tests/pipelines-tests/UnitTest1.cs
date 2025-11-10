117a95bc1b6d93fad96ced62db09ec724ba0d407
Carolina Uribe Builes <91623368+Carolinaubes@users.noreply.github.com>
2025-11-10 16:12:23 -0500

    Prueba unitaria inicial

---
+++ Tests/pipelines-tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace pipelines_tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+    }
+}
